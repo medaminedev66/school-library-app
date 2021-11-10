17808d9c239d66b52ef2abd2b08d3c4490d4da1e
Amine Smahi <md.amine.smahi@gmail.com>
2021-11-10 16:21:52 +0100

    Create a Classroom class and its relation with Student class

---
+++ classroom.rb
@@ -0,0 +1,13 @@
+class Classroom
+  def initialize (label)
+    @label=label
+    @students=[]
+  end
+
+  def add_student student
+    @students.push(student)
+    student.classroom=self
+  end
+  
+  attr_accessor :label
+end
--- student.rb
+++ student.rb
@@ -6,6 +6,13 @@ class Student < Person
     @classroom = classroom
   end
 
+  attr_accessor :classroom
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+  
   def play_hokey
     "\(ツ)/¯"
   end
