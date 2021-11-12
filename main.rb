7d0c60a478f9ee3cbe43c9c99e5b85bc412de550
Amine Smahi <md.amine.smahi@gmail.com>
2021-11-12 13:25:56 +0100

    Edit some methods and fix linters errors

--- main.rb
+++ main.rb
@@ -3,10 +3,11 @@ require './classroom'
 require './student'
 require './book'
 require './rental'
+require './teacher'
 
-$people = []
-$books = []
-$rentals = []
+@people = []
+@books = []
+@rentals = []
 
 def main
   puts "Welcome to our App \n\n"
@@ -32,44 +33,42 @@ def cases(choose)
     list_of_books
     continue
   when 2
-    list_all_peaple
+    list_all_people
     continue
   when 3
-    puts 'Do you want to create a student (1) or teacher (2)'
-    choice = gets.chomp
-    create_person(choice.to_i)
-    continue
+    create_person
   when 4
     create_book
-    continue
   when 5
     create_rental
-    continue
   when 6
     list_rentals_by_id
-    continue
   else
-    puts 'The number should be between 1 and 7'
-    start
+    continue
   end
 end
 
-def create_person(choice)
-  case choice
+def create_person
+  puts 'Do you want to create a student (1) or teacher (2)'
+  choice = gets.chomp
+  case choice.to_i
   when 1
     info = person_information
-    puts 'Classrrom'
-    get_classroom = gets.chomp
-    classroom = Classroom.new(get_classroom)
-    $people << Student.new(classroom: classroom, age: info[0], name: info[1], parent_permission: info[2])
-    puts "Student created successfully \n"
+    classroom = Classroom.new('Other')
+    @people << Student.new(classroom: classroom, age: info[0], name: info[1], parent_permission: info[2])
+    puts "Student is created successfully \n"
   when 2
-    puts 'Working on teacher'
+    info = person_information
+    puts 'Specialization:'
+    specialization = gets.chomp
+    @people << Teacher.new(specialization: specialization, age: info[0], name: info[1], parent_permission: info[2])
+    puts "Teacher is created successfully \n"
   else
-    puts 'please choose between 1 or 2'
+    puts 'please choose between 1 (Student) or 2: (Teacher)'
     choice = gets.chomp
     create_person(choice.to_i)
   end
+  continue
 end
 
 def person_information
@@ -88,65 +87,66 @@ def create_book
   title = gets.chomp
   puts 'Author: '
   author = gets.chomp
-  $books << Book.new(title, author)
+  @books << Book.new(title, author)
   puts 'Book is created succussfully'
+  continue
 end
 
 def create_rental
   puts "Select book from this books using the number: \n"
   list_of_books
   book_number = gets.chomp
-  $books.each_with_index do |book, index|
-    @selected_book = book if (index + 1) == book_number.to_i
-  end
-  # puts "title: #{@selected_book.title}, Author: #{@selected_book.author}"
+  @books.each_with_index { |book, index| @selected_book = book if (index + 1) == book_number.to_i }
   puts "Select a person fome the following list of members buy id:\n"
-  list_all_peaple
+  list_all_people
   person_id = gets.chomp
-  $people.each do |person|
-    @selected_person = person if person.id == person_id.to_i
-  end
+  @people.each { |person| @selected_person = person if person.id == person_id.to_i }
   puts 'Date:'
   date = gets.chomp
   if @selected_person && @selected_book
-    $rentals << Rental.new(date, @selected_person, @selected_book)
+    @rentals << Rental.new(date, @selected_person, @selected_book)
     puts 'Rental created seccussfully'
   else
-    puts 'something null'
+    puts 'Something went wrong. Please try again.'
+    create_rental
   end
+  continue
 end
 
-def list_all_peaple
-  if $people.size.zero?
+def list_all_people
+  if @people.size.zero?
     puts 'No people exist'
   else
     puts 'List of all members'
-    $people.each { |person| puts "Name: #{person.name} | Id: #{person.id} | Age: #{person.age} \n" }
+    @people.each do |person|
+      type = person.is_a?(Student) ? 'Student' : 'Teacher'
+      puts "[#{type}] Name: #{person.name} | Id: #{person.id} | Age: #{person.age} \n"
+    end
   end
 end
 
 def list_of_books
-  if $books.size.zero?
+  if @books.size.zero?
     puts 'No books'
   else
     puts "List of all books: \n"
-    $books.each_with_index { |book, index| puts "#{index + 1}- Title: #{book.title}, Author: #{book.author}" }
+    @books.each_with_index { |book, index| puts "#{index + 1}- Title: #{book.title}, Author: #{book.author}" }
   end
 end
 
 def list_rentals_by_id
-  puts 'Give us an id'
+  puts 'Id:'
   id = gets.chomp
-  puts 'List of all rentals for the member '
-  $people.each { |person| @selected_person = person if person.id == id }
+  @people.each { |person| @selected_person = person if person.id == id }
   if @selected_person
-    puts "List of all rentals for #{@selected_person}, id: #{@selected_person.id}"
+    puts "List of all rentals for #{@selected_person.name}, id: #{@selected_person.id}:"
     @selected_person.rentals.each do |rental|
-      puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}\n"
+      puts "- Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}\n"
     end
   else
     puts "The member with id: #{id} doesn't exist"
   end
+  continue
 end
 
 def continue
