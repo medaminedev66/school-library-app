7748542afaa085b37728de13ff99013842a58470
Amine Smahi <md.amine.smahi@gmail.com>
2021-11-12 10:13:23 +0100

    Create create_book method in main file

--- main.rb
+++ main.rb
@@ -1,8 +1,44 @@
 require './person'
 require './classroom'
 require './student'
+require './book'
 
 $people = []
+$books = []
+
+def main
+  puts "Please choose an option by enterin a number:
+      1- List all books.
+      2- List all people.
+      3- Create a person (teacher or student, not a plain Person).
+      4- Create a book.
+      5- Create a rental.
+      6- List all rentals for a given person id.
+      7- Exit"
+  choose = gets.chomp
+  cases choose
+end
+
+def cases choose
+  case choose.to_i
+  when 1
+    list_of_books
+    continue
+  when 2
+    list_all_peaple
+    continue
+  when 3
+    puts 'Do you want to create a student (1) or teacher (2)'
+    choice = gets.chomp
+    create_person(choice.to_i)
+    continue
+  when 4
+    create_book
+    main
+  else
+    puts 'The number should be between 1 and 7'
+  end
+end
 
 def person_information
   puts 'Your name'
@@ -33,11 +69,21 @@ def create_person(choice)
   end
 end
 
+def create_book
+  puts 'Title: '
+  title= gets.chomp
+  puts 'Author: '
+  author=gets.chomp
+  $books << Book.new(title, author)
+  puts "Book is created succussfully"
+end
+
 def list_all_peaple
   if $people.size.zero?
     puts 'No people exist'
   else
-    $people.each { |person| puts "List of all members \n name: #{person.name} | age: #{person.age}" }
+    puts "List of all members"
+    $people.each { |person| puts "Name: #{person.name} | Age: #{person.age} \n" }
   end
 end
 
@@ -45,44 +91,20 @@ def list_of_books
   if $books.size.zero?
     puts 'No books'
   else
-    $books.each {|book| puts "List of all books \n Title: #{book.title} | Author: #{book.author}"}
+    puts "List of all books \n"
+    $books.each {|book| puts "Title: #{book.title}, Author: #{book.author}"}
   end
 end
 
 def continue
-  puts "Do you want to continue: \n
+  puts "\nDo you want to continue:\n
   1: Continue
-  0: Exit"
+  0: Exit\n"
   continue = gets.chomp
-  main if continue == '1'
-end
-
-def main
-  puts " Welcome to school library App! \n\n Please chhose an option by enterin a number:
-      1- List all books.
-      2- List all people.
-      3- Create a person (teacher or student, not a plain Person).
-      4- Create a book.
-      5- Create a rental.
-      6- List all rentals for a given person id."
-  choose = gets.chomp
-  case choose.to_i
-  when 1
-    list_of_books
-    continue
-  when 2
-    list_all_peaple
-    continue
-  when 3
-    puts 'Do you want to create a student (1) or teacher (2)'
-    choice = gets.chomp
-    create_person(choice.to_i)
-    continue
+  if continue == '1'
+    main 
   else
-    puts 'The number should be between 1 and 7'
-    main
-  end
-  puts 'thanks'
+    puts "Thank you for using our app"
 end
 
 main
